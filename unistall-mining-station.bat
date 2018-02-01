51a688a2c4bc24af2c08c732f1fb1ef80dc9c4fe
MolecularDust <30814968+MolecularDust@users.noreply.github.com>
2018-02-01 17:47:53 +0300

    Update unistall-mining-station.bat

--- unistall-mining-station.bat
+++ unistall-mining-station.bat
@@ -1,4 +1,4 @@
 reg delete "HKEY_CURRENT_USER\Software\Mining Station" /f
 reg delete "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run" /v "Mining Station" /f
-del "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\start-miner*.lnk"
+del "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\start-miner*.lnk" /Q
 pause
