d9819ed4f3ab1d0a8e8781a0bb810a4e0349f1fd
MolecularDust <30814968+MolecularDust@users.noreply.github.com>
2018-02-01 17:37:44 +0300

    Uninstall left-overs .bat file
    This will delete Mining Station registry entries and any left-over shortcuts in system's Startup folder.

---
+++ unistall-mining-station.bat
@@ -0,0 +1,3 @@
+reg delete "HKEY_CURRENT_USER\Software\Mining Station" /f
+reg delete "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run" /v "Mining Station" /f
+del "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\start-miner*.lnk"
